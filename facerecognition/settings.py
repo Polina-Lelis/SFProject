2115d5457f9f803b4ce984a4a12d1c7048c9343a
Polina-Lelis <55255078+Polina-Lelis@users.noreply.github.com>
2021-05-30 21:08:32 +0300

    Update settings.py

--- facerecognition/settings.py
+++ facerecognition/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'facerecognition.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
- DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
